eebf3923331fd9bfd9c0b8bb37dc7beefc97a581
Dishamuk <121844371+Dishamuk@users.noreply.github.com>
2023-01-11 23:22:52 +0530

    Day01

--- src/Day01.java
+++ src/Day01.java
@@ -3,7 +3,21 @@ public class Day01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		addfunction(10,20);
+		addfunction(20,202);
+		addfunction(20,1);
 
 	}
-
+ 
+	public static void addfunction( double m , double n)
+	{
+		System.out.println(m+n);
+	}
+	public static void addfunction( int m, int n)
+	{
+		System.out.println(m+n);
+	}
 }
+;
+
+// this is called method overloading, same method,diffrent datatypes//
