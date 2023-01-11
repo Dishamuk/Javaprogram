997c1e4edaaa8f7194c12b2668583282f336fd2f
Dishamuk <121844371+Dishamuk@users.noreply.github.com>
2023-01-12 00:06:01 +0530

    Day03

---
+++ src/Day03.java
@@ -0,0 +1,17 @@
+
+public class Day03 {
+	int c= 123;
+	
+public static void main(String[]args) {
+	Day03 abc = new Day03();
+	abc.fun();
+	int main = 10;
+	System.out.println(main);
+	System.out.println("Good day");
+	System.out.println(abc.c);
+}
+ public void fun()
+ {
+	 System.out.println("Besant");
+ }
+}
