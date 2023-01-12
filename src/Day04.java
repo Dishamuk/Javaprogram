548242edbefa769b8e48c50ab7d0bb582534e9f2
Dishamuk <121844371+Dishamuk@users.noreply.github.com>
2023-01-12 09:30:17 +0530

    Day04

---
+++ src/Day04.java
@@ -0,0 +1,21 @@
+
+public class Day04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=10;
+      int b=20;
+      double c=10.00;
+      double d= 20.00;
+      
+      System.out.println("add");
+      System.out.println(a+b);
+      System.out.println("mul");
+      System.out.println(a*b);
+      System.out.println("quotient");
+      System.out.println(a/b);
+      System.out.println("remainder");
+      System.out.println(c%d);
+	}
+
+}
