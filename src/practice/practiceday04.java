13a001ef69362946d9abdc52ead1041aa6a9fa76
Dishamuk <121844371+Dishamuk@users.noreply.github.com>
2023-01-12 09:47:58 +0530

    practiceday04

---
+++ src/practice/practiceday04.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class practiceday04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int x=10;
+ int y=20;
+ double m=20.02;
+ double n=30.09;
+ 
+ System.out.println(x!=y);
+ System.out.println(x<=y); 
+ System.out.println(m>=n);
+ System.out.println(m<n);
+	}
+
+}
