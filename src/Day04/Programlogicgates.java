3bbf30ab85e47c9d1ecb9c89d6fe4f639339d358
Dishamuk <121844371+Dishamuk@users.noreply.github.com>
2023-01-15 23:08:58 +0530

    update

---
+++ src/Day04/Programlogicgates.java
@@ -0,0 +1,16 @@
+package Day04;
+
+public class Programlogicgates {
+	
+	public static void main (String[]args) {
+		int a=10;
+		int b=20;
+		int c=30;
+		
+		System.out.println(a>b && a<c);	
+		System.out.println(a>b || a<c);	
+		System.out.println(!(a>b && a<c));	
+	}
+	
+
+}
