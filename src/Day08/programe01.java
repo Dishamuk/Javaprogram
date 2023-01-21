1a1f0a14eef3ba2f187c8a6c62a723248923c854
Dishamuk <121844371+Dishamuk@users.noreply.github.com>
2023-01-21 09:32:42 +0530

    update

---
+++ src/Day08/programe01.java
@@ -0,0 +1,23 @@
+package Day08;
+
+public class programe01 {
+	
+	public static void main(String[]args ) {
+		int x=1;
+		do                                        //do while. loop(action is performed then condition is checked
+		{
+			System.out.println("hello");
+			x=x+1;
+		}
+		while(x<=5);
+		
+		
+		int  a=1;         //while .loop first condition is checked then action is performed
+		while(a<=3)
+		{
+			System.out.println("good day");
+			a=a+1;
+		}
+	}
+
+}
